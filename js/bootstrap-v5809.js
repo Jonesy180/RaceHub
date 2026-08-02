34641dfb245401495641254a1579890a6b0180d5
Jonesy180 <andyjones180@yahoo.co.uk>
2026-08-02 16:27:03 +0100

    RaceHub v5.8.16 - emergency rollback to working build

--- js/bootstrap-v5809.js
+++ js/bootstrap-v5809.js
@@ -2,4 +2,4 @@
 state=rhLoad();
 rhSync();
 if(state&&!state.onboarded){window.rhStartOnboardingIfNeeded?.();}else{show('home');}
-if('serviceWorker' in navigator){window.addEventListener('load',()=>navigator.serviceWorker.register('./service-worker-v5810.js',{updateViaCache:'none'}).catch(()=>{}));}
+if('serviceWorker' in navigator){window.addEventListener('load',()=>navigator.serviceWorker.register('./service-worker-v5816.js',{updateViaCache:'none'}).catch(()=>{}));}
