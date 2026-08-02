27da5c9a373c63c4e34593eeacc4837a8ef73929
Jonesy180 <andyjones180@yahoo.co.uk>
2026-08-02 17:01:39 +0100

    RaceHub v5.8.19 - sync update version and service worker

--- js/bootstrap-v5809.js
+++ js/bootstrap-v5809.js
@@ -2,4 +2,4 @@
 state=rhLoad();
 rhSync();
 if(state&&!state.onboarded){window.rhStartOnboardingIfNeeded?.();}else{show('home');}
-if('serviceWorker' in navigator){window.addEventListener('load',()=>navigator.serviceWorker.register('./service-worker-v5817.js',{updateViaCache:'none'}).catch(()=>{}));}
+if('serviceWorker' in navigator){window.addEventListener('load',()=>navigator.serviceWorker.register('./service-worker-v5819.js',{updateViaCache:'none'}).catch(()=>{}));}
