ffa9bbd9627a899777e3409e86327e714125ae88
Jonesy180 <andyjones180@yahoo.co.uk>
2026-08-10 19:54:00 +0100

    RaceHub v6.0.40 - clean full screen reset confirmations

--- js/bootstrap-v600.js
+++ js/bootstrap-v600.js
@@ -2,4 +2,4 @@
 state=rhLoad();
 rhSync();
 if(state&&!state.onboarded){window.rhStartOnboardingIfNeeded?.();}else{show('home');}
-if('serviceWorker' in navigator){window.addEventListener('load',()=>navigator.serviceWorker.register('./service-worker-v6039.js',{updateViaCache:'none'}).catch(()=>{}));}
+if('serviceWorker' in navigator){window.addEventListener('load',()=>navigator.serviceWorker.register('./service-worker-v6040.js',{updateViaCache:'none'}).catch(()=>{}));}
