738e08848384b74d24b8f201568d5571479271b5
Jonesy180 <andyjones180@yahoo.co.uk>
2026-08-15 20:37:55 +0100

    OTG v6.0.55 dashboard live logo layer

--- js/bootstrap-v600.js
+++ js/bootstrap-v600.js
@@ -2,4 +2,4 @@
 state=rhLoad();
 rhSync();
 if(state&&!state.onboarded){window.rhStartOnboardingIfNeeded?.();}else{show('home');}
-if('serviceWorker' in navigator){window.addEventListener('load',()=>navigator.serviceWorker.register('./service-worker-v6054.js',{updateViaCache:'none'}).catch(()=>{}));}
+if('serviceWorker' in navigator){window.addEventListener('load',()=>navigator.serviceWorker.register('./service-worker-v6055.js',{updateViaCache:'none'}).catch(()=>{}));}
