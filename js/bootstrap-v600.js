2d4ba0edb68448c348d0a82af45cd980053978d9
Jonesy180 <andyjones180@yahoo.co.uk>
2026-08-15 23:46:08 +0100

    OTG v6.0.63 final rebrand

--- js/bootstrap-v600.js
+++ js/bootstrap-v600.js
@@ -2,4 +2,4 @@
 state=rhLoad();
 rhSync();
 if(state&&!state.onboarded){window.rhStartOnboardingIfNeeded?.();}else{show('home');}
-if('serviceWorker' in navigator){window.addEventListener('load',()=>navigator.serviceWorker.register('./service-worker-v6060.js',{updateViaCache:'none'}).catch(()=>{}));}
+if('serviceWorker' in navigator){window.addEventListener('load',()=>navigator.serviceWorker.register('./service-worker-v6063.js',{updateViaCache:'none'}).catch(()=>{}));}
