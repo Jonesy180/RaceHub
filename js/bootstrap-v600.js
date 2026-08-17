d7caafada8cab1fb894e443466f3a3a22015d3f7
Jonesy180 <andyjones180@yahoo.co.uk>
2026-08-17 16:45:00 +0100

    OTG v6.0.80 fix catalogue migration update wiring

--- js/bootstrap-v600.js
+++ js/bootstrap-v600.js
@@ -2,4 +2,4 @@
 state=rhLoad();
 rhSync();
 if(state&&!state.onboarded){window.rhStartOnboardingIfNeeded?.();}else{show('home');}
-if('serviceWorker' in navigator){window.addEventListener('load',()=>navigator.serviceWorker.register('./service-worker-v6063.js',{updateViaCache:'none'}).catch(()=>{}));}
+if('serviceWorker' in navigator){window.addEventListener('load',()=>navigator.serviceWorker.register('./service-worker-v6080.js',{updateViaCache:'none'}).catch(()=>{}));}
