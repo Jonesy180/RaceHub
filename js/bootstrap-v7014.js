ef55655c074e75e4b6a055eea9e3809cf3a85a79
Jonesy180 <andyjones180@yahoo.co.uk>
2026-08-27 18:18:11 +0100

    OTG v7.0.14 - Race Setups polish

---
+++ js/bootstrap-v7014.js
@@ -0,0 +1,2 @@
+/* OTG! v7.0.14 bootstrap */
+if('serviceWorker' in navigator){window.addEventListener('load',()=>navigator.serviceWorker.register('./service-worker-v7014.js',{updateViaCache:'none'}).catch(()=>{}));}
